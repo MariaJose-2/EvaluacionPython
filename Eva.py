55bf50f63f977c8e5285a051c55a62d2df07ce73
MariaJose-2 <129073742+MariaJose-2@users.noreply.github.com>
2023-12-06 12:57:27 -0500

    Update eva

--- Eva.py
+++ Eva.py
@@ -1,58 +1,89 @@
-#creamos una clase llamada votaciones
 class Votaciones:
-    #metodo init constructor de la clase votaciones 
     def __init__(self):
-        #Inicializa un diccionario vacio para almacenar la informacion de los votantes
-        self.votantes={}
-        #Inicializa una lista para almacenar los candidatos
-        self.candidatos=['Juan Martines #10','Marcela Andrade #07', 'Agustin Olivares #25']
+        # Inicialización de atributos de la clase
+        self.votantes = {}  # Diccionario para almacenar información de los votantes (clave: ID, valor: información del votante)
+        self.votantesQueYavotaron = set()  # Conjunto para almacenar IDs de votantes que ya han votado
+        self.candidatos = ['Juan Martines #10', 'Marcela Andrade #07', 'Agustin Olivares #25', 'Voto en blanco']  # Lista de candidatos
+        self.votos = {candidato: 0 for candidato in self.candidatos}  # Diccionario para almacenar los votos por candidato (inicializado a 0)
 
-#metodo para registrar un votante con su informacion
     def registroVotante(self):
-         #Solicita al votante ingresar su Id (identificacion, numero de documento)
-        Id = input("Ingrese el numero del documento de Identidad: ")
-        #Solicita al votante ingresar sus nombres y apellidos completos
-        nombre = input("Ingrese el nombre y apellidos completo: ").lower()
-        #se almacena la informacion de vontantes en el diccionario llamado votantes
-        self.votantes[Id] = {"Nombres Completos": nombre}
-        #imprime un mensaje indicando que se ha registrado correctamente
-        print("Registro Guardado")
-        
-#metodo para validar si el votante se encuentra registrado en el sistema pasra asi poder pasar a la segunda opcion de votar
-    def validacionRegistro(self):
-        Id = input("Ingrese su Numero de documento de identidad: ")
-        if Id in self.votantes:
-            self.votantes(Id)
-        else:
-            print("No se ha registrado en el sistema")
+    # Método para registrar un votante
+        Id = input("Ingrese el numero del documento de Identidad: ")  # Solicita al usuario ingresar su número de documento de identidad
+        nombre = input("Ingrese el nombre y apellidos completos: ").lower()  # Solicita al usuario ingresar su nombre y apellidos completos
+        self.votantes[Id] = {"Nombres Completos": nombre}  # Agrega la información del votante al diccionario de votantes, usando el ID como clave
+        print("Registro Guardado")  # Imprime un mensaje indicando que el registro ha sido guardado con éxito
 
-#se crea una instancia de la clase Votaciones con el nombre de votos
+    def validacionRegistro(self):
+    # Método para validar si un votante puede votar
+        Id = input("Ingrese su Numero de documento de identidad: ")  # Solicita al usuario ingresar su número de documento de identidad
+        if Id in self.votantes:  # Verifica si el ID está en el diccionario de votantes
+                if Id not in self.votantesQueYavotaron:  # Verifica si el votante no ha votado aún
+                    self.votar(Id)  # Si el votante no ha votado, llama al método votar para permitirle votar
+                else:
+                    print("Usted ya ha votado. No puede votar más de una vez.")  # Imprime un mensaje indicando que el votante ya ha votado
+        else:
+            print("No se ha registrado en el sistema")  # Imprime un mensaje indicando que el votante no está registrado en el sistema
+
+    def votar(self, Id):
+    # Método para realizar el proceso de votación
+        print("Candidatos disponibles:")
+        for i, candidato in enumerate(self.candidatos, start=1):
+            print(f"{i}. {candidato}")
+
+        opcion = int(input("Seleccione el número del candidato al que desea votar: "))  # Solicita al usuario seleccionar un candidato mediante un número
+
+        if 1 <= opcion <= len(self.candidatos):  # Verifica si la opción seleccionada está dentro del rango de candidatos
+            candidato_votado = self.candidatos[opcion - 1]  # Obtiene el candidato correspondiente a la opción seleccionada
+            self.votos[candidato_votado] += 1  # Incrementa el contador de votos para el candidato seleccionado
+            self.votantesQueYavotaron.add(Id)  # Agrega el ID del votante a la lista de votantes que ya han votado
+            print(f"¡Voto registrado por {self.votantes[Id]['Nombres Completos']}!")  # Imprime un mensaje indicando que el voto ha sido registrado exitosamente
+        else:
+            print("Opción no válida.")  # Imprime un mensaje indicando que la opción seleccionada no es válida
+
+    def mostrarResultados(self):
+    # Método para mostrar los resultados de la votación
+        print("\nResultados de la votación:")
+        for candidato, votos in self.votos.items():
+            print(f"{candidato}: {votos} votos")  # Imprime el nombre del candidato y la cantidad de votos que ha recibido
+
+        ganador = max(self.votos, key=self.votos.get)  # Determina el candidato con el mayor número de votos
+
+        print(f"\nGanador: {ganador} con {self.votos[ganador]} votos")  # Imprime el ganador y la cantidad de votos que ha recibido
+        
+        # Verifica si hay empate entre los candidatos
+        empate = [candidato for candidato, votos in self.votos.items() if votos == self.votos[ganador] and candidato != ganador]
+
+        if empate:
+            print("\nEmpate entre los siguientes candidatos:")
+            for candidato in empate:
+                print(f"{candidato}: {self.votos[candidato]} votos")  # Imprime el candidato y la cantidad de votos que ha recibido en caso de empate
+
+# Instancia de la clase Votaciones
 votos = Votaciones()
 
-#se crea un bucle principal del programa
+# Bucle principal del programa
 while True:
-    #Imprime el menu de opciones para el usuario
+    # Muestra las instrucciones y opciones disponibles al usuario
+    print("\nPor favor seguir las instrucciones")
     print("\nOpciones")
     print("1. Registro")
-    print("2. votar")
-    print("3. Salir")
-    
-    #Solicita al votante seleccionar una opcion del menu
-    opcion = input("Seleccione una opcion: ")
+    print("2. Votar")
+    print("3. Mostrar Resultados")
+    print("4. Salir")
 
-    #Evalua la opcion seleccionada por el votante y realiza la accion correspondiente
+    # Solicita al usuario seleccionar una opción
+    opcion = input("Seleccione una opción: ")
+
+    # Evalúa la opción seleccionada por el usuario
     if opcion == '1':
-        #llama la instancia 'votos' y el nombre del metodo 'registroVotante'
-       votos.registroVotante()
-
+        votos.registroVotante()  # Llama al método de registro de votante si la opción es '1'
     elif opcion == '2':
-        #llama la instancia 'votos' y el nombre del metodo 'validacionRegistro'
-       votos.validacionRegistro()
-
+        votos.validacionRegistro()  # Llama al método de validación de registro y votación si la opción es '2'
     elif opcion == '3':
-        #Imprime un mensaje indicando que el votante salio del programa y sale del bucle
-        print("Salio del programa")
-        break #termina el programa
+        votos.mostrarResultados()  # Llama al método de mostrar resultados si la opción es '3'
+    elif opcion == '4':
+        print("Saliendo del programa")  # Imprime un mensaje indicando que el programa está saliendo
+        break  # Sale del bucle principal y termina el programa
     else:
-        #Imprime un mensaje indicando que la opcion seleccionada por el votante no es valida
-        print("Opcion no valida, elija una opcion valida")
+        print("Opción no válida, elija una opción válida")  # Imprime un mensaje si la opción seleccionada no es válida
+
